8cdf0fdfdaa450b0db0321775029ef4b1610d04b
Zaur Elbukaev <elbukaevzaur@gmail.com>
2024-01-31 14:47:28 +0300

    fix

--- src/main/resources/db/migration/V1.6__update_users_table.sql
+++ src/main/resources/db/migration/V1.6__update_users_table.sql
@@ -2,6 +2,6 @@ ALTER TABLE users
     ADD is_active boolean default true;
 
 ALTER TABLE users
-    ADD is_delete boolean default false
+    ADD is_delete boolean default false;
 
 insert into users (name, username, password, role_id) values ('admin', 'admin', 'admin', 1);
